882d989488c76718955a5c8301c798cddad1f185
Sharon Stromberg <sharon@ccharacter.com>
2023-02-01 11:21:47 -0500

    try wp_upload_dir

--- ls-gallery.php
+++ ls-gallery.php
@@ -186,6 +186,7 @@ function ls_modified_gallery_shortcode($attr) { ?>
 		$image_src_url2 = wp_get_attachment_image_src($id, 'thumbnail');
 		
 		error_log(getcwd(),0);
+		error_log(wp_upload_dir(),0);
 		
 		list($imagewidth, $imageheight) = getimagesize($image_src_url[0]);
 		$imagewidth=$image_src_url[1];
